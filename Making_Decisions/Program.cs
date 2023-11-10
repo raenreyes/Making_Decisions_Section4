f6a616d960831f8ac80b7bb6af81d4d2c9eed78f
Raen Reyes <raen.reyes11@outlook.com>
2023-11-09 19:37:27 -0800

    intro to if and else

--- Making_Decisions/Program.cs
+++ Making_Decisions/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter your weather in Celsius");
+            string input = Console.ReadLine();  
+            int temp = int.Parse(input);
+
+            if(temp < 20)
+            {
+                Console.WriteLine("Take the coat");
+            }
+            else if (temp == 20)
+            {
+                Console.WriteLine("Pants and Pull over should be fine");
+            }
+            else if (temp > 40)
+            {
+                Console.WriteLine("Stay Inside");
+            }
+            else
+            {
+                Console.WriteLine("Shorts and shirt would be good for today");
+            }
         }
     }
 }
