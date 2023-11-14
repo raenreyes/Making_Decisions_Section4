da717cc5575f9a7e3fb455629d60e166a1a1be6b
Raen Reyes <raen.reyes11@outlook.com>
2023-11-13 22:13:58 -0800

    nested if statement

--- Making_Decisions/Program.cs
+++ Making_Decisions/Program.cs
@@ -3,37 +3,25 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter your weather in Celsius");
-            string numberAsString = Console.ReadLine();
-            int temp;
+        {   
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your username:");
+            userName = Console.ReadLine();
 
-            bool success = int.TryParse(numberAsString, out temp);
-            if (success) 
-            {
-                Console.WriteLine("You gave a valid number and the temperature is " + temp + " degrres celsius");
-            }
-            else
-            {
-                temp = 0;
-                Console.WriteLine("Valued entered Invalid and the temperature is set to " + temp);
-            }
+           
 
-            if (temp < 20 )
+            if (isRegistered && userName != "")
             {
-                Console.WriteLine("Take the coat");
-            }
-            else if (temp == 20)
-            {
-                Console.WriteLine("Pants and Pull over should be fine");
-            }
-            else if (temp > 40)
-            {
-                Console.WriteLine("Stay inside way too hot");
-            }
-            else
-            {
-                Console.WriteLine("Shorts and shirt would be good for today");
+                Console.WriteLine("Hi there, registered user");
+                Console.WriteLine("Hi there, " + userName);
+
+                if (userName.Equals("admin") || userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hi there Admin");
+                    }
+                
             }
         }
     }
