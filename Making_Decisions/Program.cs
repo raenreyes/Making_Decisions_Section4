ae53b2518a25f9705e1fc49d810257447c2db24a
Raen Reyes <raen.reyes11@outlook.com>
2023-11-14 22:24:05 -0800

    ternary operator

--- Making_Decisions/Program.cs
+++ Making_Decisions/Program.cs
@@ -1,29 +1,29 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Making_Decisions
 {
     internal class Program
     {
-        //global variables are made static so we can access them without creating an instance
-        static int highScore = 80;
-        static string highScorePlayer = "Denis";
+       
         static void Main(string[] args)
         {
-            CheckScore(100,"MasterDev09");
-            
+           
+            int temp = -1;
+            string stateOfMatter;
+            //this is how you can add another expression to a ternary operator
+            stateOfMatter = temp < 0 ? "Solid" : temp > 100 ? "Gas": "Liquid"; 
+            Console.WriteLine($"The state of mater is {stateOfMatter}");
+
+            temp = 20;
+            stateOfMatter = temp < 0 ? "Solid" : temp > 100 ? "Gas" : "Liquid";
+            Console.WriteLine($"The state of mater is {stateOfMatter}");
+
+            temp = 430;
+            stateOfMatter = temp < 0 ? "Solid" : temp > 100 ? "Gas" : "Liquid";
+            Console.WriteLine($"The state of mater is {stateOfMatter}");
         }
-        public static void CheckScore(int score, string playerName) {
-            //here we are comparing our global varibles to local variables
-            if (score > highScore)
-            {
-                Console.WriteLine("The old highscore of " + highScore + 
-                 " was broken with a new score of " + score);
-                Console.WriteLine("New highscore holder is " + playerName);
-            }
-            else {
-                Console.WriteLine("The old highscore of " + highScore +
-                 " could not be broken and is still held by " + highScorePlayer);
-            }
-        }
+       
+        
     }
 }
