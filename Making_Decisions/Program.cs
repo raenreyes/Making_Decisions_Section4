b42c9405cd78268f3a8212dd32fe64e957e9bf7f
Raen Reyes <raen.reyes11@outlook.com>
2023-11-14 21:35:43 -0800

    switch statement

--- Making_Decisions/Program.cs
+++ Making_Decisions/Program.cs
@@ -3,26 +3,47 @@
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            bool isAdmin = false;
-            bool isRegistered = true;
-            string userName = "";
-            Console.WriteLine("Please enter your username:");
-            userName = Console.ReadLine();
+        {
+            int age = 17;
+            string userName = "Admin";
 
-           
 
-            if (isRegistered && userName != "")
+            CheckAge(age);
+            CheckUserName(userName);
+        }
+
+        public static void CheckAge(int age) {
+            switch (age)
             {
-                Console.WriteLine("Hi there, registered user");
-                Console.WriteLine("Hi there, " + userName);
-
-                if (userName.Equals("admin") || userName.Equals("Admin"))
-                    {
-                        Console.WriteLine("Hi there Admin");
-                    }
-                
+                case  17:
+                    Console.WriteLine("Too young to party in the club!");
+                    break;
+                case 21:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you then?");
+                    break;
             }
         }
+
+        public static void CheckUserName (string userName) {
+
+            switch (userName) {
+                case "Denis":
+                    Console.WriteLine("Hello Employee Denis!");
+                    break;
+                case "Admin":
+                    Console.WriteLine("Hello Admin!");
+                    break;
+                case "Billy":
+                    Console.WriteLine("Hello Employee Billy");
+                    break;
+                    default : 
+                    Console.WriteLine("You are not an employee here!");
+                    break;
+            }
+        }
+        
     }
 }
